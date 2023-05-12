030879f50972f94c6d7f45f238c529eb8841f75d
Carlos <carloshlzansavio@gmail.com>
2023-05-12 10:38:47 -0300

    Add GA

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,29 @@ import Head from "next/head";
 import AgIntro from "../components/agIntro";
 import AgTitle from "../components/agTitle";
 import Erase from "../components/erase";
+import Script from "next/script";
+
+const GA_TRACKING_ID = "G-Z27Y9XMDSZ";
 
 const Home: NextPage = () => {
   return (
     <div className="bg-[#524630] text-white">
-      {" "}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${GA_TRACKING_ID}');
+          `}
+      </Script>{" "}
       {/* bg-slate-900 */}
       <Head>
-        <title>Adgallery-app</title>
+        <title>Infop - São João Nepomuceno</title>
         <meta name="description" content="CZAR+ Adgallery" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
